a04372b07610308d37e20cd509915de9e2403c68
Jay Pathare <jaypathare123@gmail.com>
2026-06-25 00:09:04 -0400

    chore: inject missing next config architecture and netlify hooks

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextKey} */
+const nextConfig = {
+  reactStrictMode: true,
+  /* Advanced production build settings can be appended here */
+};
+
+module.exports = nextConfig;
